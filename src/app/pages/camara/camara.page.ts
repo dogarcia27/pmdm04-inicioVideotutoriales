404a27ab7c56a8a058e656cf20473021c89f0cb6
Dorkaitz Garcia <dogarcia@birt.eus>
2025-05-28 02:50:57 +0200

    Funcionalidad sacar foto implementada

--- src/app/pages/camara/camara.page.ts
+++ src/app/pages/camara/camara.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
 
 @Component({
   selector: 'app-camara',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CamaraPage implements OnInit {
 
+  arrayUrl: string[] = [];
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  onClick() {
+    this.takePicture();
+  }
+
+  public async takePicture() {
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: false,
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Camera
+    });
+
+    // image.webPath will contain a path that can be set as an image src.
+    // You can access the original file using image.path, which can be
+    // passed to the Filesystem API to read the raw data of the image,
+    // if desired (or pass resultType: CameraResultType.Base64 to getPhoto)
+    this.arrayUrl.push(image.webPath);
+
+};
 }
